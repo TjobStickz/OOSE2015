b124495e6fe54f75371d749079a60984b7cf0e64
TjobStickz <tjobstickz@gmail.com>
2015-05-06 10:55:46 +0200

    Grid class variables

---
+++ src/example/tGrid.java
@@ -0,0 +1,15 @@
+package example;
+
+import org.newdawn.slick.Image;
+
+public class tGrid {
+	
+	int posX;
+	int posY;
+	
+	public int[] xRow = new int [12];
+	public int[] yRow = new int [27];
+	
+	public Image gridImg;
+
+}
