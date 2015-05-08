22ff4b8ec6c56c0469522748de0d56e5f18dfe77
Minkz0r <Rochr2@gmail.com>
2015-05-08 14:20:31 +0200

    asdasda

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -213,6 +213,7 @@ public class SimpleSlickGame extends BasicGame
 			Long.sQ.draw(Long.xRow[currentBrick[i][0]],Long.yRow[currentBrick[i][1]]);	
 		NextBrickImage.Draw.drawCentered(245.0f, 65.0f);
 		
+		// Drawing border + Score and lines
 		Border.draw(0,0);
 		g.drawString("Score :", 209, 150);
 		g.drawString(" " + score, 200,170);
